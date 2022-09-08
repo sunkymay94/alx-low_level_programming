426e81b95182eb4f54c30c18e7f7f51b9e66dda4
sunkymay94 <mayolasunkanmi@gmail.com>
2022-09-08 15:39:03 -0700

    My first C language project

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,8 @@
 #include <stdio.h>
 /*
- * beginning of code
  * main - Entry point
  *
- * *Return: 0 after succesfully printing function
+ * Return: 0 after succesfully printing function
  */
 
 int main(void)
