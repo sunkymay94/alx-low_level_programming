e2719c92c513b5641869564244da788ea322afd3
sunkymay94 <mayolasunkanmi@gmail.com>
2022-09-08 15:25:11 -0700

    My first C language project

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,17 +1,12 @@
 #include <stdio.h>
-
 /*
- * 
  * * beginning of codes
  *
  * *Return: 0 after succesfully printing function
- *
  */
 
-int main()
-
-{puts("\"Programming is like building a multilingual puzzle");
-
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
-
 }
