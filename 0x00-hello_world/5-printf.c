af86e29f4d1b1265a17f5cd6bcda491960874fbe
sunkymay94 <mayolasunkanmi@gmail.com>
2022-09-08 16:07:30 -0700

    My first C language project

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,17 +1,12 @@
 #include <stdio.h>
-
 /**
- * *Beginning of code
+ * Beginning of code
+ * main - entry point
  *
- * *Return: O after successfully printing function
- *
- * */
-
-int main ()
+ * Return: O after successfully printing function
+ */
+int main (void)
 }
-
  printf ("with proper grammar, but the outcome is a piece of art, \n");
-
      return (0);
-
      }
