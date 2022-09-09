58189be9d82f2610a4a5aeeb9b31ce2c962b3bef
sunkymay94 <mayolasunkanmi@gmail.com>
2022-09-08 17:10:55 -0700

    My first C language project

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -13,10 +13,10 @@ int main(void)
 	char z;
 	float f;
 
-	printf("Size of a char: %lu bytes(s)\n", (unsigned long)sizeof(z));
-	printf("Size of an int: %lu bytes(s)\n", (unsigned long)sizeof(w));
-	printf("Size of a long int: %lu bytes(s)\n", (unsigned long)sizeof(x));
-	printf("Size of a long long int: %lu bytes(s)\n", (unsigned long)sizeof(y));
-	printf("Size of a float: %lu bytes(s)\n", (unsigned long)sizeof(f));
+	printf("Size of a char: %d bytes(s)\n", (unsigned long)sizeof(z));
+	printf("Size of an int: %d bytes(s)\n", (unsigned long)sizeof(w));
+	printf("Size of a long int: %d bytes(s)\n", (unsigned long)sizeof(x));
+	printf("Size of a long long int: %d bytes(s)\n", (unsigned long)sizeof(y));
+	printf("Size of a float: %d bytes(s)\n", (unsigned long)sizeof(f));
 	return (0);
 }
