541df5b76e43edf31ca87c63f85a9d2c941b38a7
sunkymay94 <mayolasunkanmi@gmail.com>
2022-09-09 17:04:09 -0700

    My Variable project in C

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -19,7 +19,7 @@ p = n % 10;
 
 if (p > 5)
 {
-	printf("Last digit of %d is %d and is greater tha 5\n", n, p);
+	printf("Last digit of %d is %d and is greater than 5\n", n, p);
 }
 else if (p == 0)
 {
