66e52eedf1056a3c7b0d9b1b440e06cf3138ab92
sunkymay94 <mayolasunkanmi@gmail.com>
2022-09-09 17:13:19 -0700

    My Variable project in C

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -10,9 +10,12 @@ int main(void)
 {
 char c;
 for (c = 'a' ; c <= 'z'; c++)
-	for (c = 'A' ; c <= 'Z' ; c++)
 {
 	putchar(c);
+}
+for (c = 'A' ; c <= 'Z' ; c++)
+{
+putchar(c);
 }
 putchar('\n');
 return (0);
