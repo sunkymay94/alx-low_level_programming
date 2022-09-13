18792db73b1ec018c597ee15def7bcc921045f9c
sunkymay94 <mayolasunkanmi@gmail.com>
2022-09-13 15:33:19 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -11,9 +11,11 @@ int _abs(int);
 int print_last_digit(int);
 void jack_bauer(void);
 void times_table(void);
-int add(int a, int b)
-void print_to_98(int n);
+int add(int a , int b);
 void print_times_table(int n);
-void num(int r, int n);
+void print_to_98(int n);
+void num(int r , int n);
 
 #endif
+
+
