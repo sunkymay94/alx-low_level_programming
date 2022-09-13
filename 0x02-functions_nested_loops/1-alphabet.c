8999847f986a14c8273aebc5570cc4cd3afab441
sunkymay94 <mayolasunkanmi@gmail.com>
2022-09-13 15:54:58 -0700

    Nested loop function task

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_alphabet - A function to print abc
+ * Return: Always Success
+ */
+void print_alphabet(void)
+{
+	char e;
+	
+	for (e='a'; e<='z'; e++)
+	{
+		_putchar(e);
+	}
+_putchar('\n');
+}
---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - A function to print the abc 10 times
+ * Return: Always Success
+ */
+void print_alphabet_x10(void)
+{
+	char e,j;
+
+	for (j=; j<=9; j++)
+	{
+	
+	for (e='a'; e<='z'; e++)
+	{
+		_putchar(e);
+	}
+	_putchar('\n');
+	}
+}
