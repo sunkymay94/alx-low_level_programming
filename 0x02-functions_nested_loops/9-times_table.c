448a14487a2d052e76b6f3eae81bcdc605b21a72
sunkymay94 <mayolasunkanmi@gmail.com>
2022-09-13 17:48:43 -0700

    Nested loop commit

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -18,6 +18,8 @@ for (num = 0; num <= 9; num++)
 		_putchar(' ');
 		prod = num * mult;
 		if (prod <= 9)
+			_putchar(' ');
+		else
 		_putchar((prod / 10) + '0');
 		_putchar((prod % 10) + '0');
 		}
