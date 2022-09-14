02f763ca6079ce75624bd3be29d6af5ce23702e4
sunkymay94 <mayolasunkanmi@gmail.com>
2022-09-13 17:34:10 -0700

    Nested loop commit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_last_digit - A function that prints the last digit of a number
+ * @n: will be produce as the number
+ * Return: Always Success
+ */
+int print_last_digit(int n)
+{
+	int last_digit = n % 10;
+
+	if (last_digit < 0)
+	last_digit * = -1;
+
+	_putchar(last_digit = '0');
+	return (last_digit);
+}
