30536494aa3989955615b0a962429806df8615d4
sunkymay94 <mayolasunkanmi@gmail.com>
2022-09-19 16:14:30 -0700

    Pointers, arrays and string project

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,15 @@
+#icnlude "main.h"
+
+/**
+* _puts - write string followed by new line
+* @str: string
+ * Return: void
+*/
+
+void _puts(char *str)
+{
+	while (*str)
+		_putchar(*str++);
+
+	_putchar('\n');
+}
