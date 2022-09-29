8f89f94d177ff572580f5d7eaf61682cfc9564f0
sunkymay94 <mayolasunkanmi@gmail.com>
2022-09-29 14:09:41 -0700

    Recursion project commit

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -8,8 +8,8 @@
  *	 If the number is not divisible - 1.
  */
 
-int is_divisible(int num, int div);
-int is_prime_number(int n);
+int is_divisible(int num, int div)
+int is_prime_number(int n)
 {
 	if (num % div == 0)
 		return (0);
